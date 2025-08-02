38611b1b9230581f41dc2a495171df8046807461
Lyannadu26 <colson19862@gmail.com>
2025-08-02 22:21:56 +0200

    Écrasement du dépôt distant avec ma version locale

--- src/main/java/fr/mtraddon/Main.java
+++ src/main/java/fr/mtraddon/Main.java
@@ -7,6 +7,5 @@ public class Main implements ModInitializer {
     @Override
     public void onInitialize() {
         System.out.println("MTR France Addon chargé !");
-        // Tu peux ajouter ici l’enregistrement de blocs, items, etc.
     }
 }
