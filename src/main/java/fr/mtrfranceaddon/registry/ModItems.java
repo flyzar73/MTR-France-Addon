d6d04c400ac00bf5610c66d48ec986093a561bbd
Adel MERLE <mrl.adel.sanorel@gmail.com>
2025-08-07 18:58:20 +0200

    refactor(core)!: Réécriture de la base du projet
    Réécriture partielle de la base du projet avec différents fichiers initiaux (inspiré du code source de l'addon JCM)
    
    - Suppression du DataGen
    - Le Client n'est plus séparée du Core
    - Refonte du build.gradle et sa mise en forme
    - Ajout de deux blocs test comme exemples : "mtrfranceaddon_logo_block" et "test_platform".
    
    BREAKING CHANGE: Ancienne structure non opérationnel avec celle-ci. Une grande partie du code a été modifiée.

---
+++ src/main/java/fr/mtrfranceaddon/registry/ModItems.java
@@ -0,0 +1,11 @@
+package fr.mtrfranceaddon.registry;
+
+import fr.mtrfranceaddon.MTRFranceAddon;
+
+public class ModItems {
+
+    public static void register() {
+        MTRFranceAddon.LOGGER.info("Registering Items");
+    }
+
+}
