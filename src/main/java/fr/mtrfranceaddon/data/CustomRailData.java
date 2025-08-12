2ec9421dce2b157600b4a21b24e14fbd00d4a81a
Flyzar73 <61349213+flyzar73@users.noreply.github.com>
2025-08-12 15:42:15 +0200

    TGV 395 + Rail update

--- src/main/java/fr/mtrfranceaddon/data/CustomRailData.java
+++ src/main/java/fr/mtrfranceaddon/data/CustomRailData.java
@@ -20,16 +20,16 @@ public final class CustomRailData {
     }
 
     public static final List<RailInfo> CUSTOM_RAILS = Arrays.asList(
-            new RailInfo("RAIL_30", 30, MapColor.getStoneGrayMapped()),
-            new RailInfo("RAIL_50", 50, MapColor.getStoneGrayMapped()),
-            new RailInfo("RAIL_70", 70, MapColor.getStoneGrayMapped()),
-            new RailInfo("RAIL_90", 90, MapColor.getStoneGrayMapped()),
-            new RailInfo("RAIL_110", 110, MapColor.getStoneGrayMapped()),
-            new RailInfo("RAIL_130", 130, MapColor.getStoneGrayMapped()),
-            new RailInfo("RAIL_150", 150, MapColor.getStoneGrayMapped()),
-            new RailInfo("RAIL_220", 220, MapColor.getStoneGrayMapped()),
-            new RailInfo("RAIL_270", 270, MapColor.getStoneGrayMapped()),
-            new RailInfo("RAIL_320", 320, MapColor.getStoneGrayMapped()),
+            new RailInfo("RAIL_30", 30, MapColor.getSpruceBrownMapped()),
+            new RailInfo("RAIL_50", 50, MapColor.getDarkGreenMapped()),
+            new RailInfo("RAIL_70", 70, MapColor.getDarkRedMapped()),
+            new RailInfo("RAIL_90", 90, MapColor.getLightBlueGrayMapped()),
+            new RailInfo("RAIL_110", 110, MapColor.getTerracottaOrangeMapped()),
+            new RailInfo("RAIL_130", 130, MapColor.getLimeMapped()),
+            new RailInfo("RAIL_150", 150, MapColor.getBlueMapped()),
+            new RailInfo("RAIL_220", 220, MapColor.getGoldMapped()),
+            new RailInfo("RAIL_270", 270, MapColor.getOrangeMapped()),
+            new RailInfo("RAIL_320", 320, MapColor.getPinkMapped()),
             new RailInfo("RAIL_350", 350, MapColor.getStoneGrayMapped())
     );
 
