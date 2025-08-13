a732665139d4d9bca751f16b14afaa58fd978969
Noob-at-everything <tinael.gousset@hotmail.com>
2025-08-13 15:37:47 +0200

    Ajout des Block SNCF

--- src/main/java/fr/mtrfranceaddon/registry/ModBlocks.java
+++ src/main/java/fr/mtrfranceaddon/registry/ModBlocks.java
@@ -25,6 +25,47 @@ public final class ModBlocks {
             () -> new Block(new BlockPlatform(createDefaultBlockSettings(false), false)),
             ItemGroups.MTRFranceAddonGroup
     );
+    public static final BlockRegistryObject SNCF_ACTUAL_LOGO = MTRFranceAddonRegistry.registerBlockWithItem(
+            "sncf_logo_actuel_block",
+            () -> new Block(new LogoBlock(new BlockSettings(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)))),
+            ItemGroups.MTRFranceAddonGroup
+    );
+    public static final BlockRegistryObject SNCF_1992TO2005_LOGO = MTRFranceAddonRegistry.registerBlockWithItem(
+            "sncf_logo_1992-2005_block",
+            () -> new Block(new LogoBlock(new BlockSettings(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)))),
+            ItemGroups.MTRFranceAddonGroup
+    );
+    public static final BlockRegistryObject SNCF_1985TO1992_LOGO_V1 = MTRFranceAddonRegistry.registerBlockWithItem(
+            "sncf_logo_1985-1992_block_v1",
+            () -> new Block(new LogoBlock(new BlockSettings(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)))),
+            ItemGroups.MTRFranceAddonGroup
+    );
+    public static final BlockRegistryObject SNCF_1985TO1992_LOGO_V2 = MTRFranceAddonRegistry.registerBlockWithItem(
+            "sncf_logo_1985-1992_block_v2",
+            () -> new Block(new LogoBlock(new BlockSettings(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)))),
+            ItemGroups.MTRFranceAddonGroup
+    );
+    public static final BlockRegistryObject SNCF_1967TO1985_LOGO_V1 = MTRFranceAddonRegistry.registerBlockWithItem(
+            "sncf_logo_1967-1985_block_v1",
+            () -> new Block(new LogoBlock(new BlockSettings(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)))),
+            ItemGroups.MTRFranceAddonGroup
+    );
+    public static final BlockRegistryObject SNCF_1967TO1985_LOGO_V2 = MTRFranceAddonRegistry.registerBlockWithItem(
+            "sncf_logo_1967-1985_block_v2",
+            () -> new Block(new LogoBlock(new BlockSettings(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)))),
+            ItemGroups.MTRFranceAddonGroup
+    );
+    public static final BlockRegistryObject SNCF_1947TO1967_LOGO = MTRFranceAddonRegistry.registerBlockWithItem(
+            "sncf_logo_1947-1967_block",
+            () -> new Block(new LogoBlock(new BlockSettings(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)))),
+            ItemGroups.MTRFranceAddonGroup
+    );
+    public static final BlockRegistryObject SNCF_1938TO1947_LOGO = MTRFranceAddonRegistry.registerBlockWithItem(
+            "sncf_logo_1938-1947_block",
+            () -> new Block(new LogoBlock(new BlockSettings(AbstractBlock.Settings.copy(Blocks.WHITE_WOOL)))),
+            ItemGroups.MTRFranceAddonGroup
+    );
+
 
     public static void register() {
         MTRFranceAddon.LOGGER.info("Registering Blocks");
